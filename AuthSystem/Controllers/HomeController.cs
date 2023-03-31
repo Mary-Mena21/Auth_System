117e507ac9ffc50174bce0d5872c0092ac47a75f
Mariana Mena <maranashokry@ahoo.com>
2023-03-31 16:32:58 -0500

    Authentication login is working!

--- AuthSystem/Controllers/HomeController.cs
+++ AuthSystem/Controllers/HomeController.cs
@@ -1,9 +1,11 @@
 ﻿using AuthSystem.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
 namespace AuthSystem.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
